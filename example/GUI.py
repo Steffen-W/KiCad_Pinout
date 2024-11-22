ed1932226ce19d9950f5628e5e005eff8d8ece47
Steffen Wittemeier <uni@steffenwittemeier.de>
2024-11-22 15:34:27 +0100

    first working version

---
+++ example/GUI.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+
+###########################################################################
+## Python code generated with wxFormBuilder (version 4.2.1-0-g80c4cb6)
+## http://www.wxformbuilder.org/
+##
+## PLEASE DO *NOT* EDIT THIS FILE!
+###########################################################################
+
+import wx
+import wx.xrc
+
+###########################################################################
+## Class GUI_Dialog
+###########################################################################
+
+class GUI_Dialog ( wx.Dialog ):
+
+	def __init__( self, parent ):
+		wx.Dialog.__init__ ( self, parent, id = wx.ID_ANY, title = u"Pinout result", pos = wx.DefaultPosition, size = wx.Size( 500,500 ), style = wx.DEFAULT_DIALOG_STYLE )
+
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+
+		gbSizer1 = wx.GridBagSizer( 0, 0 )
+		gbSizer1.SetFlexibleDirection( wx.BOTH )
+		gbSizer1.SetNonFlexibleGrowMode( wx.FLEX_GROWMODE_SPECIFIED )
+
+		bSizer2 = wx.BoxSizer( wx.VERTICAL )
+
+		bSizer4 = wx.BoxSizer( wx.HORIZONTAL )
+
+		self.m_staticText11 = wx.StaticText( self, wx.ID_ANY, u"Select output format", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText11.Wrap( -1 )
+
+		bSizer4.Add( self.m_staticText11, 0, wx.ALIGN_CENTER|wx.ALL, 5 )
+
+		output_formatChoices = [ u"List", u"CSV", u"HTML table", u"Markdown table", u"C/C++ code (#define)", u"C/C++ code (enum)", u"Python (dict)", u"WireViz ", u"FPGA (XDC format)", u"FPGA (PDC format)" ]
+		self.output_format = wx.Choice( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, output_formatChoices, 0 )
+		self.output_format.SetSelection( 0 )
+		bSizer4.Add( self.output_format, 1, wx.ALL, 5 )
+
+		self.m_buttonUpdate = wx.Button( self, wx.ID_ANY, u"Update", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer4.Add( self.m_buttonUpdate, 0, wx.ALL, 5 )
+
+
+		bSizer2.Add( bSizer4, 1, wx.EXPAND, 5 )
+
+		self.m_staticline1 = wx.StaticLine( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LI_HORIZONTAL )
+		bSizer2.Add( self.m_staticline1, 0, wx.ALL|wx.EXPAND, 5 )
+
+		gSizer1 = wx.GridSizer( 0, 2, 0, 0 )
+
+		self.m_staticText2 = wx.StaticText( self, wx.ID_ANY, u"Start sequence\n{reference}, {value}, {description}", wx.DefaultPosition, wx.DefaultSize, wx.ALIGN_CENTER_HORIZONTAL )
+		self.m_staticText2.Wrap( -1 )
+
+		gSizer1.Add( self.m_staticText2, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+
+		self.m_text_start = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE )
+		gSizer1.Add( self.m_text_start, 1, wx.ALL|wx.EXPAND, 5 )
+
+		self.m_staticText6 = wx.StaticText( self, wx.ID_ANY, u"Pin sequence\n{pin_function}, {netname}, \n{number}, {pin_type}, {connected}", wx.DefaultPosition, wx.DefaultSize, wx.ALIGN_CENTER_HORIZONTAL )
+		self.m_staticText6.Wrap( -1 )
+
+		gSizer1.Add( self.m_staticText6, 0, wx.ALL|wx.ALIGN_CENTER_HORIZONTAL|wx.ALIGN_CENTER_VERTICAL, 5 )
+
+		self.m_text_pin = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE )
+		gSizer1.Add( self.m_text_pin, 0, wx.ALL|wx.EXPAND, 5 )
+
+		self.m_staticText7 = wx.StaticText( self, wx.ID_ANY, u"End sequence\n{reference}, {value}, {description}", wx.DefaultPosition, wx.DefaultSize, wx.ALIGN_CENTER_HORIZONTAL )
+		self.m_staticText7.Wrap( -1 )
+
+		gSizer1.Add( self.m_staticText7, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL|wx.ALIGN_CENTER_HORIZONTAL, 5 )
+
+		self.m_text_end = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE )
+		gSizer1.Add( self.m_text_end, 0, wx.ALL|wx.EXPAND, 5 )
+
+
+		bSizer2.Add( gSizer1, 0, wx.EXPAND, 5 )
+
+
+		gbSizer1.Add( bSizer2, wx.GBPosition( 1, 0 ), wx.GBSpan( 1, 1 ), wx.EXPAND, 5 )
+
+		self.result = wx.TextCtrl( self, wx.ID_ANY, wx.EmptyString, wx.DefaultPosition, wx.DefaultSize, wx.TE_MULTILINE|wx.TE_READONLY|wx.VSCROLL )
+		self.result.SetFont( wx.Font( 10, wx.FONTFAMILY_TELETYPE, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_NORMAL, False, "Monospace" ) )
+		self.result.SetToolTip( u"Result (copy-paste this)" )
+
+		gbSizer1.Add( self.result, wx.GBPosition( 0, 0 ), wx.GBSpan( 1, 1 ), wx.ALL|wx.EXPAND, 5 )
+
+
+		gbSizer1.AddGrowableCol( 0 )
+		gbSizer1.AddGrowableRow( 0 )
+
+		self.SetSizer( gbSizer1 )
+		self.Layout()
+
+		self.Centre( wx.BOTH )
+
+		# Connect Events
+		self.m_buttonUpdate.Bind( wx.EVT_BUTTON, self.update )
+
+	def __del__( self ):
+		pass
+
+
+	# Virtual event handlers, override them in your derived class
+	def update( self, event ):
+		event.Skip()
+
+
--- example/Plugin_action.py
+++ example/Plugin_action.py
@@ -1,59 +1,218 @@
 import pcbnew
 import os
 import wx
-import time
+import pprint
+import re
 
 if __name__ == "__main__":
-    from Plugin_gui import MyDialog
-    from Plugin_event import *
+    from GUI import GUI_Dialog
 else:
-    from .Plugin_gui import MyDialog
-    from .Plugin_event import *
+    from .GUI import GUI_Dialog
+
+SELECTOR = [
+    "list",
+    "python",
+    "csv",
+    "html",
+    "md",
+    "c_define",
+    "c_enum",
+    "python_dict",
+    "wireviz",
+    "fpga_xdc",
+    "fpga_pdc",
+]
 
 
-class KiCadPlugin(MyDialog):
+def validate_format_string(format_string: str, placeholders: list):
+    """
+    Validates that all placeholders in the format string are allowed by placeholders.
 
-    def __init__(self, board, action):
+    Args:
+        format_string (str): The format string to validate.
+        placeholders (list): A list of valid placeholder names.
+    """
+
+    # Find all placeholders in the format string using a regex
+    placeholders_in_string = re.findall(r"\{(\w+)\}", format_string)
+
+    # Check if any placeholders are not in the allowed list
+    unexpected_placeholders = [
+        placeholder
+        for placeholder in placeholders_in_string
+        if placeholder not in placeholders
+    ]
+    if unexpected_placeholders:
+        return f"The format string contains unexpected placeholders: {', '.join(unexpected_placeholders)}"
+    return ""
+
+
+def format_pins(data, start_seq, pin_seq, end_seq):
+    output = []
+    for item in data:
+        # Start
+        error = validate_format_string(start_seq, ["reference", "value", "description"])
+        if error:
+            print(f"Validation Error: {error}")
+        else:
+            output.append(
+                start_seq.format(
+                    reference=item["Reference"],
+                    value=item["Value"],
+                    description=item["description"],
+                )
+            )
+
+        # Main
+        error = validate_format_string(
+            pin_seq, ["pin_function", "netname", "number", "pin_type", "connected"]
+        )
+        if error:
+            print(f"Validation Error: {error}")
+        else:
+            for pin in item["pins"]:
+                output.append(
+                    pin_seq.format(
+                        pin_function=pin.get("PinFunction", ""),
+                        netname=pin.get("Netname", ""),
+                        number=pin.get("Number", ""),
+                        pin_type=pin.get("PinType", ""),
+                        connected=pin.get("Connected", False),
+                    )
+                )
+
+        # End
+        error = validate_format_string(end_seq, ["reference", "value", "description"])
+        if error:
+            print(f"Validation Error: {error}")
+        else:
+            output.append(
+                end_seq.format(
+                    veference=item["Reference"],
+                    value=item["Value"],
+                    description=item["description"],
+                )
+            )
+
+    return "\n".join(output)
+
+
+def get_pins(component: pcbnew.FOOTPRINT):
+    pinout = []
+    seen_numbers = set()  # to avoid duplicates
+    for pad in component.Pads():
+        if isinstance(pad, pcbnew.PAD):
+            number = pad.GetNumber()
+            if number in seen_numbers:
+                continue
+            else:
+                seen_numbers.add(number)
+
+            pinout.append(
+                {
+                    "Number": number,
+                    "PinFunction": pad.GetPinFunction(),
+                    "PinType": pad.GetPinType(),
+                    "Netname": pad.GetNetname(),
+                    "Connected": pad.IsConnected(),
+                }
+            )
+    return pinout
+
+
+class KiCadPlugin(GUI_Dialog):
+
+    def __init__(self, board: pcbnew.BOARD, action: pcbnew.ActionPlugin):
         super(KiCadPlugin, self).__init__(None)
+        self.Bind(wx.EVT_MENU, self.on_escape, id=wx.ID_CLOSE)
+        self.Bind(wx.EVT_CHAR_HOOK, self.on_key_press)
+
         self.board = board
         self.action = action
-        self.config = {}
-        EVT_UPDATE(self, self.updateDisplay)
-        self.Thread = PluginThread(self)
+        self.config = {}  # todo
 
-    def run(self, event):  # Button in GUI
-        self.m_checkBox.SetLabelText("Working...")
-        time.sleep(1)
-        wx.MessageBox("Done", parent=self)
-        self.m_checkBox.SetLabelText("Done")
+        self.output_format.Bind(wx.EVT_CHOICE, self.update)
+        # self.pinNameCB.Bind(wx.EVT_CHECKBOX, self.update)
+        # self.pinNameFilter.Bind(wx.EVT_TEXT, self.update)
 
-    def on_close(self, event):
-        self.Thread.stopThread = True
-        self.EndModal(wx.ID_OK)
+        self.output_format.Set(SELECTOR)
+        self.output_format.SetSelection(0)
 
-    def updateDisplay(self, status):
-        # pcbnew.Refresh()
-        temp = status.data
-        self.m_staticText.SetLabelText(temp)
+        start_seq = "// {reference} {value}"
+        pin_seq = "#define {pin_function} {netname}"
+        end_seq = "//"
+        self.m_text_start.Clear()
+        self.m_text_start.WriteText(start_seq)
+        self.m_text_pin.Clear()
+        self.m_text_pin.WriteText(pin_seq)
+        self.m_text_end.Clear()
+        self.m_text_end.WriteText(end_seq)
+
+        self.update()
+
+    def update(self, event=None):
+        sel_type = self.output_format.GetStringSelection()
+        print("change_format", "sel_type", sel_type)
+
+        footprint_list = []
+        for footprint in self.board.GetFootprints():
+            if isinstance(footprint, pcbnew.FOOTPRINT):
+                if footprint.IsSelected():
+
+                    Value = footprint.GetValue()  # like ESP32-S2FH4
+                    Reference = footprint.GetReference()  # like IC1
+                    description = ""  # footprint.GetLibDescription()
+                    pins = get_pins(footprint)
+
+                    if not pins:
+                        continue
+
+                    footprint_list.append(
+                        {
+                            "Value": Value,
+                            "Reference": Reference,
+                            "description": description,
+                            "pins": pins,
+                        }
+                    )
+        pptext = pprint.pformat(footprint_list)
+        print(pptext)
+
+        start_seq = self.m_text_start.GetValue()
+        pin_seq = self.m_text_pin.GetValue()
+        end_seq = self.m_text_end.GetValue()
+
+        formatted_output = format_pins(footprint_list, start_seq, pin_seq, end_seq)
+        self.result.Clear()
+        self.result.WriteText(formatted_output)
+
+        if event:
+            event.Skip()
+
+    def on_escape(self, event):
+        self.Close()
+
+    def on_key_press(self, event):
+        if event.GetKeyCode() == wx.WXK_ESCAPE:
+            self.Close()
+        else:
+            event.Skip()
 
 
 class ActionKiCadPlugin(pcbnew.ActionPlugin):
     def defaults(self):
-        self.name = "name"
-        self.category = "category"
-        self.description = "description"
+        self.name = "Pinout Generator"
+        self.category = "Read PCB"
+        self.description = "Generates pinout exports from the PCB nets"
         self.show_toolbar_button = True
-        self.icon_file_name = os.path.join(
-            os.path.dirname(__file__), "icon.png")
-        self.dark_icon_file_name = os.path.join(
-            os.path.dirname(__file__), "icon.png")
+        self.icon_file_name = os.path.join(os.path.dirname(__file__), "icon.png")
+        self.dark_icon_file_name = os.path.join(os.path.dirname(__file__), "icon.png")
 
     def Run(self):
         board = pcbnew.GetBoard()
         Plugin_h = KiCadPlugin(board, self)
         Plugin_h.ShowModal()
         Plugin_h.Destroy()
-        pcbnew.Refresh()
 
 
 if __name__ == "__main__":
